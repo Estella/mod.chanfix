483aae4bb53d68f928b20a0f205ffe1a98ab6729
Compster <devnull@localhost>
2005-12-29 02:47:09 +0000

    Oops, forgot to add the help hostmask/account changes

--- doc/chanfix.help.sql
+++ doc/chanfix.help.sql
@@ -9,6 +9,7 @@ ADDFLAG	1	Adds this flag to the user. Possible flags:\nb - can block/unblock cha
 ADDHOST	1	Adds this hostmask to the user's list of hostmasks.
 ADDNOTE	1	Adds a note to a channel.
 ADDUSER	1	Adds a new user, without flags, and optionally with this hostmask.
+ALERT	1	Puts a channel on ALERT status\nChannels in alert status have information contained in notes that should be considered before fixing the channel. These channels also require the OVERRIDE flag to manually fix them.
 BLOCK	1	Blocks a channel from being fixed, both automatically and manually.\nThe reason will be shown when doing INFO <channel>.
 CHECK	1	Shows the number of ops and total clients in <channel>.
 CSCORE	1	Shows the same as SCORE, but in a compact output. See /msg C HELP SCORE.
@@ -21,14 +22,15 @@ QUOTE	1	This command should never be used.\nUsing it would be bad.\nHow bad?\nTr
 HELP	1	Shows help about <command>.
 HISTORY	1	Shows the times that <channel> has been manually fixed.
 INFO	1	Shows all notes of this channel, and whether it has been blocked.
-OPLIST	1	Shows a list of hostmasks plus their score of the top ops of this channel.
+OPLIST	1	Shows a list of accounts plus their score of the top ops of this channel.
 OPNICKS	1	Shows the nicks of all ops in <channel>.
 REHASH	1	Reloads config and users.
 RELOAD	1	Completely reloads Chanfix.
-SCORE	1	Without extra arguments, shows the top scores of <channel>.\nOtherwise, it shows the score of either the currently online client <nick>, or <hostmask> for <channel>.
+SCORE	1	Without extra arguments, shows the top scores of <channel>.\nOtherwise, it shows the score of either the currently online client <nick>, or <*account> for <channel>.
 SET	1	Sets <setting> to value <value>.\nBoolean settings: ENABLE_AUTOFIX, ENABLE_CHANFIX, ENABLE_CHANNEL_BLOCKING.\nInteger settings: NUM_SERVERS.
 STATUS	1	Shows current status.
 SUSPEND	1	Suspends the specified user.
+UNALERT	1	Removes a channel from alert status
 UNBLOCK	1	Removes the block on a channel.
 USET	1	Sets options on your account. Possible settings:\nNOTICE - receive notices, if no then PRIVMSG's will be sent.\nLANG - What language Chanfix will communicate with you in; available languages: EN - English.
 WHOIS	1	Shows information about this user.
